75f12dd6afa76d46999727b826483657fca82a7c
rodrigorvix <rodrigovitoriense@gmail.com>
2022-01-24 20:25:28 -0300

    create migration to users

---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email:Sequelize.STRING,
+        password:Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+  checkPassword(senha) {
+     return password === this.password;
+  }
+}
+
+export default User;
